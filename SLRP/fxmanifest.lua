18f8c5207760c5d4a534adc1d49332dddb117045
FluffyCarrot1 <78351832+FluffyCarrot1@users.noreply.github.com>
2022-04-18 22:19:38 +0100

    Add files via upload

---
+++ SLRP/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+author ' F.carrot'
+description 'Custom Scripting Package For General FiveM Usage'
+Version '1.0.3'
+url 'https://github.com/FluffyCarrot1'
+shared_script '@es_extended/imports.lua'
+client_scripts {
+    "*.lua"
+}
+
+
+server_scripts {
+    "*.lua"
+}
